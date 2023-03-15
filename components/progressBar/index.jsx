51f3b4e56f8f3cd17e5c09e9964fb3bc6b91aaff
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-03-15 03:05:03 +0300

    Update index.jsx

--- components/progressBar/index.jsx
+++ components/progressBar/index.jsx
@@ -18,20 +18,6 @@ function ProgressBar() {
             <span className={progress >= 9 ? `border-2 border-transparent text-[10px] bg-${progressColor[8]}-500 rounded-full p-1 px-2` : `border-2 border-transparent text-[10px] bg-slate-700 rounded-full p-1 px-2`}>9</span>
             <span className={progress >= 10 ? `border-2 border-transparent text-[10px] bg-${progressColor[9]}-500 rounded-full p-1 px-2` : `border-2 border-transparent text-[10px] bg-slate-700 rounded-full p-1 px-2`}>10</span>
         </div>
-        // {/* <div className='rounded-3xl w-[calc(100%+80px)] h-1 flex mb-8'>
-        //         <span className={progress >= 1 && `hidden bg-red-500 h-full w-[10%]  rounded-l-3xl border-slate-900`}>1</span>
-        //         <span className={progress >= 1 && `hidden bg-green-500 h-full w-[10%]  rounded-l-3xl border-slate-900`}>2</span>
-        //         <span className={progress >= 1 && `bg-${progressColor[0]}-500 h-full w-[10%] rounded-l-3xl border-slate-900`}></span>
-        //         <span className={progress >= 2 && `bg-${progressColor[1]}-500 h-full w-[10%] `}></span>
-        //         <span className={progress >= 3 && `bg-${progressColor[2]}-500 h-full w-[10%]`}></span>
-        //         <span className={progress >= 4 && `bg-${progressColor[3]}-500 h-full w-[10%]`}></span>
-        //         <span className={progress >= 5 && `bg-${progressColor[4]}-500 h-full w-[10%]`}></span>
-        //         <span className={progress >= 6 && `bg-${progressColor[5]}-500 h-full w-[10%]`}></span>
-        //         <span className={progress >= 7 && `bg-${progressColor[6]}-500 h-full w-[10%]`}></span>
-        //         <span className={progress >= 8 && `bg-${progressColor[7]}-500 h-full w-[10%]`}></span>
-        //         <span className={progress >= 9 && `bg-${progressColor[8]}-500 h-full w-[10%]`}></span>
-        //         <span className={progress >= 10 && `bg-${progressColor[9]}-500 h-full w-[10% rounded-r-3xl`}></span>
-        //     </div> */}
     )
 }
 
