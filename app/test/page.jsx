d4b39c959bc96d4f309989fa513b7635e81cef78
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-03-16 08:48:46 +0300

    Update page.jsx

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,41 +1,111 @@
 'use client'
-import { useState, useEffect } from "react";
+import React, { useRef } from "react";
+import { animateScroll as scroll } from "react-scroll";
 
-export default function MyForm() {
-    const [formHeight, setFormHeight] = useState("100vh");
+const ExampleForm = () => {
+    const firstNameRef = useRef(null);
+    const lastNameRef = useRef(null);
+    const emailRef = useRef(null);
 
-    useEffect(() => {
-        function handleResize() {
-            setFormHeight(
-                window.innerHeight > 768 ? "80vh" : "100vh"
-            );
-        }
-        window.addEventListener("resize", handleResize);
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // handle form submission logic
+    };
+
+    const scrollToRef = (ref) => {
+        scroll.scrollTo(ref.current.offsetTop - 16);
+    };
 
     return (
-        <form
-            className="flex flex-col items-center justify-end mb-20 gap-4"
-            style={{ height: formHeight }}
-        >
-            <input type="text" className="text-red-500" />
-            <input type="text" className="text-red-500" />
-            <input type="text" className="text-red-500" />
-            <input type="text" className="text-red-500" />
-            <div>
-                <label htmlFor="message">Mesajınız:</label>
-                <textarea
-                    id="message"
-                    name="message"
-                    className="border p-2"
-                    rows="5"
-                    cols="50"
-                />
-            </div>
-
-        </form>
+        <div className="min-h-screen flex items-center justify-center bg-white">
+            <form
+                className="bg-gray-100 rounded-lg p-6 w-full max-w-md"
+                onSubmit={handleSubmit}
+            >
+                <h2 className="text-2xl mb-4">Example Form</h2>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="firstName">
+                        First Name
+                    </label>
+                    <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="firstName"
+                        type="text"
+                        ref={firstNameRef}
+                        onFocus={() => scrollToRef(firstNameRef)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="lastName">
+                        Last Name
+                    </label>
+                    <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="lastName"
+                        type="text"
+                        ref={lastNameRef}
+                        onFocus={() => scrollToRef(lastNameRef)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                        Email Address
+                    </label>
+                    <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        ref={emailRef}
+                        onFocus={() => scrollToRef(emailRef)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="firstName">
+                        First Name
+                    </label>
+                    <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="firstName"
+                        type="text"
+                        ref={firstNameRef}
+                        onFocus={() => scrollToRef(firstNameRef)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="lastName">
+                        Last Name
+                    </label>
+                    <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="lastName"
+                        type="text"
+                        ref={lastNameRef}
+                        onFocus={() => scrollToRef(lastNameRef)}
+                    />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 font-bold mb-2" htmlFor="email">
+                        Email Address
+                    </label>
+                    <input
+                        className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="email"
+                        type="email"
+                        ref={emailRef}
+                        onFocus={() => scrollToRef(emailRef)}
+                    />
+                </div>
+                <div className="flex items-center justify-end">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
     );
-}
+};
+
+export default ExampleForm;
