d9a278c7c103d1c43f8e9c49c0f9b873acc0a4dc
Furkan Leylek <furkanyasinn.1@outlook.com>
2023-03-16 08:41:16 +0300

    Update page.jsx

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -18,10 +18,24 @@ export default function MyForm() {
 
     return (
         <form
-            className="flex flex-col items-center justify-end mb-20"
+            className="flex flex-col items-center justify-end mb-20 gap-4"
             style={{ height: formHeight }}
         >
             <input type="text" className="text-red-500" />
+            <input type="text" className="text-red-500" />
+            <input type="text" className="text-red-500" />
+            <input type="text" className="text-red-500" />
+            <div>
+                <label htmlFor="message">Mesajınız:</label>
+                <textarea
+                    id="message"
+                    name="message"
+                    className="border p-2"
+                    rows="5"
+                    cols="50"
+                />
+            </div>
+
         </form>
     );
 }
